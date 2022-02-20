914ea2ad4f9b628957082306583cbf262972408d
vijayakumar <vijayakumar.r@fundsindia.com>
2022-02-20 10:42:45 +0530

    initial comit

---
+++ output/MtoPInterview-Simulator.xcarchive/Products/Library/Frameworks/MtoPInterview.framework/Headers/MtoPInterview.h
@@ -0,0 +1,18 @@
+//
+//  MtoPInterview.h
+//  MtoPInterview
+//
+//  Created by vijay kumar on 09/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MtoPInterview.
+FOUNDATION_EXPORT double MtoPInterviewVersionNumber;
+
+//! Project version string for MtoPInterview.
+FOUNDATION_EXPORT const unsigned char MtoPInterviewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MtoPInterview/PublicHeader.h>
+
+
